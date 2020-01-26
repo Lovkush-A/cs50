519f96f6736f68a4090529cf305346253e768449
Lovkush <lovkush@gmail.com>
2020-01-26 01:47:09 +0000

    Update problem_set4a_helpers.h
    included edge cases for blurry

--- problem_set4a_helpers.h
+++ problem_set4a_helpers.h
@@ -16,8 +16,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width]);
 void copy(int height, int width, RGBTRIPLE image[height][width], RGBTRIPLE temp[height][width]);
 
 //create 3x3 sub-array of array of image
-void sub_array_3x3(int height, int width, RGBTRIPLE temp[height][width], int row, int column, RGBTRIPLE temp_3x3[3][3]);
+//returns "size" of sub-array
+//size is 9 for subararys from centre of images, size of 4 for subarrays from corners, size of 6 for subarrays from sides.
+int sub_array_3x3(int height, int width, RGBTRIPLE temp[height][width], int row, int column, RGBTRIPLE temp_3x3[3][3]);
 
 //multiply kernel with 3x3 sub-array.
 int mult_kernel_sub_array(int kernel[3][3], RGBTRIPLE temp_3x3[3][3], char colour);
-
