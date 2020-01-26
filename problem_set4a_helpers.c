28782fdd391d73d2bdcee591c1d3cbb0bd2c8412
Lovkush <lovkush@gmail.com>
2020-01-26 01:43:32 +0000

    Update problem_set4a_helpers.c
    included edge cases

--- problem_set4a_helpers.c
+++ problem_set4a_helpers.c
@@ -77,37 +77,47 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     //create variable kernel to multiply with temp_3x3
     int kernel[3][3] = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
 
+    //variable size_of_sub_array
+    int n = 9;
+
     //go through array and calculate new rgb values
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
             //create sub array centred on pixel i-j
-            sub_array_3x3(height, width, temp, i, j, temp_3x3);
+            n = sub_array_3x3(height, width, temp, i, j, temp_3x3);
 
-            //for each colour, multiply kernel with sub array and divide by 9.
-            image[i][j].rgbtBlue = mult_kernel_sub_array(kernel, temp_3x3, 'b') / 9;
-            image[i][j].rgbtGreen = mult_kernel_sub_array(kernel, temp_3x3, 'g') / 9;
-            image[i][j].rgbtRed = mult_kernel_sub_array(kernel, temp_3x3, 'r') / 9;
+            //for each colour, multiply kernel with sub array and divide by n.
+            image[i][j].rgbtBlue = mult_kernel_sub_array(kernel, temp_3x3, 'b') / n;
+            image[i][j].rgbtGreen = mult_kernel_sub_array(kernel, temp_3x3, 'g') / n;
+            image[i][j].rgbtRed = mult_kernel_sub_array(kernel, temp_3x3, 'r') / n;
 
-            //NEED TO FIX FACT THAT YOU SHOULDN:T ALWAYS DIVIDE BY 9!!!!
-            //NEED TO FIX FACT THAT YOU SHOULDN:T ALWAYS DIVIDE BY 9!!!!
-            //NEED TO FIX FACT THAT YOU SHOULDN:T ALWAYS DIVIDE BY 9!!!!
+            /*checks checks checks
+            if (i == 0 && j == 5)
+            {
+            for (int k = 1; k < 3; k++)
+            {
+                for (int m = 0; m < 3; m++)
+                {
+                    printf("%i %i\n", temp_3x3[k][m].rgbtBlue, temp[i - 1 + k][j - 1 + m].rgbtBlue);
+                }
+            }
+
+            printf("%i %i %i\n", image[i][j].rgbtBlue, image[i][j].rgbtGreen, image[i][j].rgbtRed);*/
+
+            /*if (image[i][j].rgbtBlue == 0 && image[i][j].rgbtGreen == 0 && image[i][j].rgbtRed == 0 && i == 0)
+            {
+                printf("i is %i, j is %i\n", i ,j);
+            }
+
+            if (image[i][j].rgbtBlue == 23 & image[i][j].rgbtGreen == 56 && image[i][j].rgbtRed == 38)
+            {
+                printf("special i is %i, j is %i\n", i ,j);
+            }*/
 
         }
     }
-
-/*    //checks checks checks
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            printf("%i %i\n", temp_3x3[i][j].rgbtBlue, temp[5 - 1 + i][5 - 1 + j].rgbtBlue);
-        }
-    }
-
-    int test = mult_kernel_sub_array(kernel, temp_3x3, 'b');
-    printf("%i\n", test); */
 
     //free memory
     free(temp);
@@ -143,35 +153,103 @@ void copy(int height, int width, RGBTRIPLE image[height][width], RGBTRIPLE temp[
 
 
 //create 3x3 sub-array of array of image
-void sub_array_3x3(int height, int width, RGBTRIPLE temp[height][width], int row, int column, RGBTRIPLE temp_3x3[3][3])
+//returns "size" of sub-array
+//size is 9 for subararys from centre of images, size of 4 for subarrays from corners, size of 6 for subarrays from sides.
+int sub_array_3x3(int height, int width, RGBTRIPLE temp[height][width], int row, int column, RGBTRIPLE temp_3x3[3][3])
 {
-    //case 0. centre of image
-    if (0 < row && row < height - 1 && 0 < column && column < width - 1)
+    //reset values of sub array to zero
+    for (int i = 0; i < 3; i++)
     {
-        for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
         {
-            for (int j = 0; j < 3; j++)
-            {
-                temp_3x3[i][j].rgbtBlue = temp[row - 1 + i][column - 1 + j].rgbtBlue;
-                temp_3x3[i][j].rgbtGreen = temp[row - 1 + i][column - 1 + j].rgbtGreen;
-                temp_3x3[i][j].rgbtRed = temp[row - 1 + i][column - 1 + j].rgbtRed;
-            }
+            temp_3x3[i][j].rgbtBlue = 0;
+            temp_3x3[i][j].rgbtGreen = 0;
+            temp_3x3[i][j].rgbtRed = 0;
         }
     }
-    //....
-    else if (0 == 1)
+
+    //create default bounds and value for n
+    int imin = 0;
+    int imax = 2;
+    int jmin = 0;
+    int jmax = 2;
+    int n = 9;
+
+    //adjust imin, imax, jmin, jmax and depending on which edge we're on, if any.
+    if (0 < row && row < height - 1) //middle rows
     {
-        for (int i = 0; i < 3; i++)
+        //left column
+        if (0 == column)
         {
-            for (int j = 0; j < 3; j++)
-            {
-                temp_3x3[i][j].rgbtBlue = temp[row - 1 + i][column - 1 + j].rgbtBlue;
-                temp_3x3[i][j].rgbtGreen = temp[row - 1 + i][column - 1 + j].rgbtGreen;
-                temp_3x3[i][j].rgbtRed = temp[row - 1 + i][column - 1 + j].rgbtRed;
-            }
+            jmin = 1;
+            n = 6;
+        }
+        //right column
+        else if (column == width - 1)
+        {
+            jmax = 1;
+            n = 6;
         }
     }
-    return;
+    //top row
+    else if (0 == row)
+    {
+        imin = 1;
+
+        //middle columns
+        if (0 < column && column < width - 1)
+        {
+            n = 6;
+        }
+        //left column
+        else if (0 == column)
+        {
+            jmin = 1;
+            n = 4;
+        }
+        //right column
+        else if (column == width - 1)
+        {
+            jmax = 1;
+            n = 4;
+        }
+    }
+    //bottom row
+    else if (row == height - 1)
+    {
+        imax = 1;
+
+        //middle columns
+        if (0 < column && column < width - 1)
+        {
+            n = 6;
+        }
+        //left column
+        else if (0 == column)
+        {
+            jmin = 1;
+            n = 4;
+        }
+        //right column
+        else if (column == width - 1)
+        {
+            jmax = 1;
+            n = 4;
+        }
+    }
+
+    //create sub array
+    for (int i = imin; i <= imax; i++)
+    {
+        for (int j = jmin; j <= jmax; j++)
+        {
+            temp_3x3[i][j].rgbtBlue = temp[row - 1 + i][column - 1 + j].rgbtBlue;
+            temp_3x3[i][j].rgbtGreen = temp[row - 1 + i][column - 1 + j].rgbtGreen;
+            temp_3x3[i][j].rgbtRed = temp[row - 1 + i][column - 1 + j].rgbtRed;
+        }
+    }
+
+    return n;
 }
 
 
