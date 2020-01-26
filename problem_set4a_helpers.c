356721c740cd334450aec62bc0bf91ee5e82e381
Lovkush <lovkush@gmail.com>
2020-01-26 12:10:47 +0000

    Update problem_set4a_helpers.c
    1) Added function declarations. They used to be in file helpers.h
    2) Added code for function edge
    3) Fixed rounding bugs

--- problem_set4a_helpers.c
+++ problem_set4a_helpers.c
@@ -1,6 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "helpers.h"
+#include <math.h> //needed for square root
+
+
+//declare functions
+//Create copy of image
+void copy(int height, int width, RGBTRIPLE image[height][width], RGBTRIPLE temp[height][width]);
+
+//create 3x3 sub-array of array of image
+//returns "size" of sub-array
+//size is 9 for subarrays from centre of images, size of 4 for subarrays from corners, size of 6 for subarrays from sides.
+int sub_array_3x3(int height, int width, RGBTRIPLE temp[height][width], int row, int column, RGBTRIPLE temp_3x3[3][3]);
+
+//multiply kernel with 3x3 sub-array.
+int mult_kernel_sub_array(int kernel[3][3], RGBTRIPLE temp_3x3[3][3], char colour);
+
+//do sobel operation on 3x3 sub-array. returns new rgb value
+int sobel(RGBTRIPLE temp_3x3[3][3], char colour);
+
+
+
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -9,7 +29,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j].rgbtBlue = (int) (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / (int) 3;
+            image[i][j].rgbtBlue = (int) round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / (float) 3);
             image[i][j].rgbtGreen = image[i][j].rgbtBlue;
             image[i][j].rgbtRed = image[i][j].rgbtBlue;
         }
@@ -66,7 +86,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     //copy image into temp
     copy(height, width, image, temp);
 
-    //create new variable/pointer temp_3x3 which will store 3x3 sub arrays
+    //create new variable temp_3x3 which will store 3x3 sub arrays
     RGBTRIPLE(*temp_3x3)[3] = calloc(3, 3*sizeof(RGBTRIPLE));
     if (temp_3x3 == NULL)
     {
@@ -75,9 +95,12 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     }
 
     //create variable kernel to multiply with temp_3x3
+    //this kernel is equivalent to calculating the sum of the values of sub-array
     int kernel[3][3] = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
 
-    //variable size_of_sub_array
+    //variable n stores size of sub array.
+    //either 9 or 6 or 4 depending on if pixel is on corner or edge or middle of image
+    //required for determining average. we will divide total by n
     int n = 9;
 
     //go through array and calculate new rgb values
@@ -89,33 +112,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             n = sub_array_3x3(height, width, temp, i, j, temp_3x3);
 
             //for each colour, multiply kernel with sub array and divide by n.
-            image[i][j].rgbtBlue = mult_kernel_sub_array(kernel, temp_3x3, 'b') / n;
-            image[i][j].rgbtGreen = mult_kernel_sub_array(kernel, temp_3x3, 'g') / n;
-            image[i][j].rgbtRed = mult_kernel_sub_array(kernel, temp_3x3, 'r') / n;
-
-            /*checks checks checks
-            if (i == 0 && j == 5)
-            {
-            for (int k = 1; k < 3; k++)
-            {
-                for (int m = 0; m < 3; m++)
-                {
-                    printf("%i %i\n", temp_3x3[k][m].rgbtBlue, temp[i - 1 + k][j - 1 + m].rgbtBlue);
-                }
-            }
-
-            printf("%i %i %i\n", image[i][j].rgbtBlue, image[i][j].rgbtGreen, image[i][j].rgbtRed);*/
-
-            /*if (image[i][j].rgbtBlue == 0 && image[i][j].rgbtGreen == 0 && image[i][j].rgbtRed == 0 && i == 0)
-            {
-                printf("i is %i, j is %i\n", i ,j);
-            }
-
-            if (image[i][j].rgbtBlue == 23 & image[i][j].rgbtGreen == 56 && image[i][j].rgbtRed == 38)
-            {
-                printf("special i is %i, j is %i\n", i ,j);
-            }*/
-
+            image[i][j].rgbtBlue = (int) round(mult_kernel_sub_array(kernel, temp_3x3, 'b') / (float) n);
+            image[i][j].rgbtGreen = (int) round(mult_kernel_sub_array(kernel, temp_3x3, 'g') / (float) n);
+            image[i][j].rgbtRed = (int) round(mult_kernel_sub_array(kernel, temp_3x3, 'r') / (float) n);
         }
     }
 
@@ -131,6 +130,45 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    //create new variable temp which will store copy of image
+    RGBTRIPLE(*temp)[width] = calloc(height, width * sizeof(RGBTRIPLE));
+    if (temp == NULL)
+    {
+        printf("assigning memory for temp failed\n");
+        return;
+    }
+
+    //copy image into temp
+    copy(height, width, image, temp);
+
+    //create new variable temp_3x3 which will store 3x3 sub arrays
+    RGBTRIPLE(*temp_3x3)[3] = calloc(3, 3*sizeof(RGBTRIPLE));
+    if (temp_3x3 == NULL)
+    {
+        printf("assigning memory for temp_3x3 failed\n");
+        return;
+    }
+
+
+    //go through array and calculate new rgb values
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            //create sub array centred on pixel i-j
+            sub_array_3x3(height, width, temp, i, j, temp_3x3);
+
+            //for each colour, multiply kernel with sub array and divide by n.
+            image[i][j].rgbtBlue = sobel(temp_3x3, 'b');
+            image[i][j].rgbtGreen = sobel(temp_3x3, 'g');
+            image[i][j].rgbtRed = sobel(temp_3x3, 'r');
+        }
+    }
+
+    //free memory
+    free(temp);
+    free(temp_3x3);
+
     return;
 }
 
@@ -293,4 +331,23 @@ int mult_kernel_sub_array(int kernel[3][3], RGBTRIPLE temp_3x3[3][3], char colou
     }
 
     return sum;
+}
+
+
+
+//sobel operator.
+int sobel(RGBTRIPLE temp_3x3[3][3], char colour)
+{
+    //create variable for the two kernels
+    int kernelx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    int kernely[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+
+    //calculate Gx and Gy
+    float Gx = mult_kernel_sub_array(kernelx, temp_3x3, colour);
+    float Gy = mult_kernel_sub_array(kernely, temp_3x3, colour);
+
+    //calculate new rgb value
+    int temp_value = (int) fmin(255, round(sqrt(Gx*Gx + Gy*Gy)));
+
+    return temp_value;
 }
